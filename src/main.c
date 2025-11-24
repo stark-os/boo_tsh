68b04484ba2210c1fe5bcdb92352f384c8541aa3
iasebsil83 <i.a.sebsil83@gmail.com>
2025-11-24 20:52:21 +0100

    added some specific behavior

--- src/main.c
+++ src/main.c
@@ -1,25 +1,76 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+
+
+
+
+
+// ---------------- DEFINITIONS ----------------
+
+//ext asm <<<<<<<<<<<<<<<<<<<<<< syscalls and get rid of includes ?
 int real_waitid(idtype_t idtype, id_t id, siginfo_t *infop, int options, void*);
 
+//IO
+#define STDIN  0
+#define STDOUT 1
+
+
+
+
+
+
+// ---------------- TOOLS ----------------
+
+//str
+static int streq(char* s1, char* s2, int len) {
+	for(int i=0; i < len; i++){
+		if(s1[i] != s2[i]){ return 0; }
+	}
+	return 1;
+}
+
+
+
+
+
+
+// ---------------- EXECUTION ----------------
+
+//main
 int _start() {
-	char command[255];
+	char cmd[255];
+
+	//main loop
 	for (;;) {
-		write(1, "# ", 2);
-		int count = read(0, command, 255);
-		// /bin/ls\n -> /bin/ls\0
-		command[count - 1] = 0;
+		write(STDOUT, "> ", 2);
+
+		//read
+		int cnt = read(STDIN, cmd, 255);
+
+		//special behaviors
+		if(cnt >= 4){
+			if(streq(cmd, "exit", 4)){ _exit(0); }
+			if(streq(cmd, "heyo", 4)){ write(STDOUT, "Hej man ! What's up ?\n", 22); }
+		}
+
+		//set null terminator in cmd
+		cmd[cnt-1] = '\0';
+
+		//sub-process
 		pid_t fork_result = fork();
-		if (fork_result == 0) {
-			execve(command, 0, 0);
+		if(fork_result == 0){
+			execve(cmd, 0, 0); //no args given
 			break;
-		} else {
-			// wait
+		}
+
+		//main process (wait until end of sub-process)
+		else{
 			siginfo_t info;
 			real_waitid(P_ALL, 0, &info, WEXITED, 0);
 		}
 	}
 
+	//end
 	_exit(0);
 }
